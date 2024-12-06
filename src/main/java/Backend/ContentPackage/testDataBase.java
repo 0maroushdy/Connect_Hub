d0e1531d79ee254b3085bf1d938ab7809f1488c1
Moustafa-Alnashar <moustafa.mohamed.alnashar@gmail.com>
2024-12-06 15:51:28 +0200

    added some testing to ContentDataBase

--- src/main/java/Backend/ContentPackage/testDataBase.java
+++ src/main/java/Backend/ContentPackage/testDataBase.java
@@ -4,13 +4,24 @@
  */
 package Backend.ContentPackage;
 
+import Backend.UserPackage.User;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+
 /**
  *
  * @author moustafa
  */
 public class testDataBase {
-    public static void main(String[] args) {
-        ContentDataBase.getInstance();
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        
+        
+        User u = User.UserFactory.create("moustafa@gmail.com","moustafa","123",LocalDate.now());
+        (new Story(u)).uplode();
+        
+        System.out.println(ContentDataBase.getInstance().getStories());
+        
+        
         
         ContentDataBase.getInstance().shutDown();
     }
