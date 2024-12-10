44fd91f9f3b92123c5a936db06b6265c1d047386
Moustafa-Alnashar <moustafa.mohamed.alnashar@gmail.com>
2024-12-11 01:50:57 +0200

    added most of the functionality as per the assignment and added the builder pattern

--- src/main/java/GroupPackage/Group.java
+++ src/main/java/GroupPackage/Group.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 TODO
 .- every photo that will be saved to the datases should get copied to an
 enternal folder
+.- make a good interface for checking for permissions
  */
 /**
  *
@@ -19,23 +20,23 @@ enternal folder
  */
 public class Group {
 
-    private UUID id;
+    private final UUID id;
     private String name;
     private String description;
     private String photoPath;
 
-    private User mainAdmin;
-    private ArrayList<User> admins;
-    private ArrayList<User> members;
+    private final User mainAdmin;
+    private final ArrayList<User> admins;
+    private final ArrayList<User> members;
 
-    public Group(String name, User mainAdmin, String description, String photoPath, UUID id) {
+    private final ArrayList<User> joinRequests;
+
+    private Group(String name, User mainAdmin, String description, String photoPath, UUID id) {
         if (name == null || name.equals("") || mainAdmin == null) {
             throw new IllegalArgumentException("Name and mainAdmin cannot be null or empty.");
         }
 
-        if (id == null) {
-            this.id = UUID.randomUUID();
-        }
+        this.id = (id == null) ? UUID.randomUUID() : id;
 
         this.name = name;
         this.mainAdmin = mainAdmin;
@@ -47,24 +48,41 @@ public class Group {
 
         this.admins = new ArrayList<>();
         this.members = new ArrayList<>();
+        this.joinRequests = new ArrayList<>();
 
         this.admins.add(mainAdmin);
         this.members.add(mainAdmin);
     }
+    
+    public Group(Group.Builder groupBuilder){
+        this(groupBuilder.name,
+                groupBuilder.mainAdmin,
+                groupBuilder.description,
+                groupBuilder.photoPath,
+                groupBuilder.id);
+    }
+
+    private void validateInputUsers(User actor, User target) {
+        if (actor == null || target == null) {
+            throw new IllegalArgumentException("Actor and target cannot be null.");
+        }
+        if (actor == target) {
+            throw new IllegalArgumentException("Actor and target cannot be the same user.");
+        }
+    }
 
     public void promote(User actor, User target) {
-        if (actor == target) {
-            System.out.println("both actor and target are the same user");
-            return;
-        }
+        validateInputUsers(actor, target);
 
+        //check for minimun permissions of actor to perform the action
         if (actor != this.mainAdmin) {
-            System.out.println("User: " + target.getUsername()
+            System.out.println("User: " + actor.getUsername()
                     + " doesn't have permission");
             return;
         }
+        //
 
-        if (!this.admins.contains(target)) {
+        if (this.admins.contains(target)) {
             System.out.println("User: " + target.getUsername()
                     + " is already an admin");
         }
@@ -79,20 +97,19 @@ public class Group {
     }
 
     public void demote(User actor, User target) {
-        if (actor == target) {
-            System.out.println("both actor and target are the same user");
-            return;
-        }
+        validateInputUsers(actor, target);
 
+        //check for minimun permissions of actor to perform the action
         if (actor != this.mainAdmin) {
-            System.out.println("User: " + target.getUsername()
+            System.out.println("User: " + actor.getUsername()
                     + " doesn't have permission");
             return;
         }
+        //
 
         if (!this.admins.contains(target)) {
             System.out.println("User: " + target.getUsername()
-                    + " is not in the group");
+                    + " is not an admin");
             return;
         }
 
@@ -100,22 +117,122 @@ public class Group {
     }
 
     public void removeMember(User actor, User target) {
-        if (actor == target) {
-            System.out.println("both actor and target are the same user");
-            return;
-        }
+        validateInputUsers(actor, target);
 
-        if (actor != this.mainAdmin) {
-            System.out.println("User: " + target.getUsername()
+        //check for minimun permissions of actor to perform the action
+        if (this.admins.contains(actor)) {
+            System.out.println("User: " + actor.getUsername()
                     + " doesn't have permission");
             return;
         }
+        //
 
-        this.admins.remove(target);
+        if (actor == this.mainAdmin) {
+            this.admins.remove(target);
+        }
 
         if (!this.members.remove(target)) {
             System.out.println("User: " + target.getUsername()
                     + " is not in the group");
+        }
+    }
+
+    public void approveJoinRequest(User actor, User target) {
+        validateInputUsers(actor, target);
+
+        //check for minimun permissions of actor to perform the action
+        if (!this.admins.contains(actor)) {
+            System.out.println("User: " + actor.getUsername()
+                    + " doesn't have permission");
+            return;
+        }
+        //
+
+        if (this.members.contains(target)) {
+            System.out.println("User: " + target.getUsername()
+                    + " is already in the group");
+            return;
+        }
+
+        if (!this.joinRequests.remove(target)) {
+            System.out.println("User: " + target.getUsername()
+                    + " didn't request to join");
+            return;
+        }
+
+        this.members.add(target);
+    }
+
+    public void rejectJoinRequest(User actor, User target) {
+        validateInputUsers(actor, target);
+
+        //check for minimun permissions of actor to perform the action
+        if (!this.admins.contains(actor)) {
+            System.out.println("User: " + actor.getUsername()
+                    + " doesn't have permission");
+            return;
+        }
+        //
+
+        if (this.members.contains(target)) {
+            System.out.println("User: " + target.getUsername()
+                    + " is already in the group");
+            return;
+        }
+
+        if (!this.joinRequests.remove(target)) {
+            System.out.println("User: " + target.getUsername()
+                    + " didn't request to join");
+            return;
+        }
+    }
+
+    public void leave(User actor) {
+        if (actor == null) {
+            throw new IllegalArgumentException("actor can't be null");
+        }
+        if (actor == this.mainAdmin) {
+            throw new IllegalArgumentException("The group's main admin can't leave the group");
+        }
+
+        this.admins.remove(actor);
+
+        if (!this.members.remove(actor)) {
+            throw new IllegalArgumentException("User: " + actor.getUsername()
+                    + " is not in the group");
+        }
+    }
+
+    public class Builder {
+
+        private UUID id;
+        private User mainAdmin;
+        private String name;
+
+        private String description;
+        private String photoPath;
+
+        public Builder(User mainAdmin, String name) {
+            if (name == null || name.equals("") || mainAdmin == null) {
+                throw new IllegalArgumentException("Name and mainAdmin cannot be null or empty.");
+            }
+            this.mainAdmin = mainAdmin;
+            this.name = name;  
+        }
+
+        public Builder setId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setPhotoPath(String photoPath) {
+            this.photoPath = photoPath;
+            return this;
         }
     }
 }
